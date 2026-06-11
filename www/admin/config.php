13d487cca227048cda6560eaf886c55f3bc600a6
palagyta_andriy <palch29101985@gmail.com>
2026-06-11 21:36:44 +0300

    fix: 5 коміт

--- www/admin/config.php
+++ www/admin/config.php
@@ -11,7 +11,7 @@ define('HTTPS_CATALOG', 'http://localhost:8181/');
 define('DIR_APPLICATION', '/var/www/admin/');
 define('DIR_SYSTEM', '/var/www/system/');
 define('DIR_IMAGE', '/var/www/image/');
-define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_STORAGE', '/var/storage/');
 define('DIR_CATALOG', '/var/www/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
--- www/config.php
+++ www/config.php
@@ -9,7 +9,7 @@ define('HTTPS_SERVER', 'http://localhost:8181/');
 define('DIR_APPLICATION', '/var/www/catalog/');
 define('DIR_SYSTEM', '/var/www/system/');
 define('DIR_IMAGE', '/var/www/image/');
-define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_STORAGE', '/var/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
